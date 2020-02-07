dbedec0b7df579260d82fdc81c6615a9fa2cc6be
sschoedel <sschoedel@vt.edu>
2020-02-06 21:39:27 -0500

    product manager and product classes

--- product.py
+++ product.py
@@ -10,7 +10,7 @@ class ProductManager(object):
 		self.URLs = URLs
 		self.products = []
 
-	def calculateScore(keyWords, discoveredWords, price):
+	def calculateScore(self, keyWords, discoveredWords, price):
 		numWords = len(discoveredWords)	
 		score = 0
 		# Make sure bot found something
@@ -27,25 +27,25 @@ class ProductManager(object):
 			# TODO: Calculate price component if word part passes, otherwise return score 0
 			# Price scoring requires knowledge of average prices for that part
 			# Search through all other listings with similar descriptions and 
-			# if wordScore >= wordThreshold:
+			# if wordScore >= self.wordThreshold:
 			# 	wordScore -= price
 			# 	score = wordScore
 
 		return score
 
 	def addProduct(self, name, link, price, website, keyWords, discoveredWords):
-		score = calculateScore(keyWords, discoveredWords, price)
-		if score >= overallThreshold:
+		score = self.calculateScore(keyWords, discoveredWords, price)
+		if score >= self.overallThreshold:
 			product = Product(name, link, price, website, score)
-			products.append(product)
+			self.products.append(product)
 
-	class Product(object):
-		def __init__ (self, name, link, price, website):
-			self.name = name
-			self.link = link
-			self.price = price
-			self.website = website
-			self.score = score
+class Product(object):
+	def __init__ (self, name, link, price, website, score):
+		self.name = name
+		self.link = link
+		self.price = price
+		self.website = website
+		self.score = score
 
-		def info(self):
-			return [self.name, self.link, self.price, self.website]
+	def printInfo(self):
+		print(f'name: {self.name}, link: {self.link}, price: {self.price}, website: {self.website}, score: {self.score}')
